29de6d3df043e46e4b3f6ae84980f7ca3d575234
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-11 13:37:43 +0100

    add routing configuration for home, map, fullcalendar, and charts components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MapComponent } from './pages/map/map.component';
+import { FullcalendarComponent } from './pages/fullcalendar/fullcalendar.component';
+import { ChartsComponent } from './pages/charts/charts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '', pathMatch: 'full', redirectTo: '/home' },
+{path: 'home', component: HomeComponent},
+{path: 'map', component: MapComponent },
+{path: 'callendar', component: FullcalendarComponent},
+{path:'charts', component: ChartsComponent},
+{path: '', redirectTo: 'home', pathMatch: 'full'},
+{path: '**', redirectTo: ''}
+];
