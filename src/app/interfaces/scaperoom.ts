3e422dedfbf904a55baa635669db64d9a28dfd41
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-17 21:52:39 +0100

    Rename the Scaperoom interface to ScapeRoomItem and add a new Scaperoom interface to structure the API response.

--- src/app/interfaces/scaperoom.ts
+++ src/app/interfaces/scaperoom.ts
@@ -1,7 +1,15 @@
-export interface Scaperoom {
+export interface ScapeRoomItem {
     id: number;
     title?: string;
     director?: string;
     poster?: string;
     genre?: string;
 }
+
+export interface Scaperoom {
+    code: number;
+    message: string;
+    data: ScapeRoomItem[]; // ✅ Ahora `data` es un array de objetos dinámico
+}
+
+
