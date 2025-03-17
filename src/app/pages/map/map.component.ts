678b438a3a6934ffca2ccd383e4b62c8bddb0a66
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-17 23:42:18 +0100

    implement map component with Leaflet integration and styling

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import * as L from 'leaflet'; // ✅ Importa Leaflet
 
 @Component({
   selector: 'app-map',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './map.component.html',
-  styleUrl: './map.component.scss'
+  styleUrls: ['./map.component.scss']
 })
-export class MapComponent {
+export class MapComponent implements AfterViewInit {
+private map!: L.Map;
+ngAfterViewInit(): void {
+  this.initMap();
+}
 
+private initMap(): void {
+  if (!document.getElementById('map')) {
+    console.error('❌ Error: The #map element does not exist.');
+    return;
+  }
+  this.map = L.map('map').setView([41.3851, 2.1734], 13); // ✅ Inicializa el mapa
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(this.map);
+
+L.marker([41.4667, 2.2803]).addTo(this.map);
+}
 }
