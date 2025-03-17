3c1f4d0d945363125843886e2fd9ed96ef58d2bb
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-17 21:54:27 +0100

    Update the getListScapeRooms method to return a single Scaperoom and add a method to delete a Scaperoom

--- src/app/services/scaperoom.service.ts
+++ src/app/services/scaperoom.service.ts
@@ -10,12 +10,47 @@ import { Scaperoom } from '../interfaces/scaperoom';
 export class ScaperoomService {
 private myAppUrl: string;
 private myApiUrl: string;
+//private listScapeRooms: Scaperoom = [];
+
   constructor(private http: HttpClient) {
     this.myAppUrl= environment.endpoint;
     this.myApiUrl= 'api/scaperoom/';
    }
 
-   getListScapeRooms(): Observable<Scaperoom[]>{
-   return this.http.get<Scaperoom[]>(this.myAppUrl + this.myApiUrl);
+   getListScapeRooms(): Observable<Scaperoom> {
+   return this.http.get<Scaperoom>(this.myAppUrl + this.myApiUrl);
+  }
+
+  deleteScapeRoom(id: number):Observable<void> {
+    return this.http.delete<void>(this.myAppUrl + this.myApiUrl + id);
+  }
+
+/*async getListScapeRooms(): Promise<Scaperoom[]> {
+  try {
+    const response = await fetch((this.myAppUrl + this.myApiUrl), {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    if (!response.ok) {
+      console.warn("Error en la respuesta de l'API");
+      return this.listScapeRooms;
+    }
+
+    const data = await response.json();
+
+    if (!data.data || data.data.length === 0) {
+      console.warn('No se encontraron más naves.');
+      return this.listScapeRooms;
+    }
+
+    return this.listScapeRooms;
+  } catch (error) {
+    console.error('Ha habido un error:', error);
+    throw error;
+  }
 }
+*/
 }
