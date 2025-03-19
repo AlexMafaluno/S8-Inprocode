10f31985f2873b42d459b87d012bd322bac568d4
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-19 12:20:07 +0100

    Add CrudView component

---
+++ src/app/pages/crud-view/crud-view.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-view',
+  imports: [],
+  templateUrl: './crud-view.component.html',
+  styleUrl: './crud-view.component.scss'
+})
+export class CrudViewComponent {
+
+}
