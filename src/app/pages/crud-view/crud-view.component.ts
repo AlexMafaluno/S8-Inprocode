ee9ef90f88ead1ea5d652c8dbcd601eb5d54aa3b
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-20 11:23:47 +0100

    Add modal functionality to CRUD view for adding new ScapeRooms

--- src/app/pages/crud-view/crud-view.component.ts
+++ src/app/pages/crud-view/crud-view.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { ListScaperoomsComponent } from '../../components/list-scaperooms/list-scaperooms.component';
+import { RouterOutlet } from '@angular/router';
+import { ModalLayoutComponent } from '../../Layouts/modal-layout/modal-layout.component';
+import { CommonModule } from '@angular/common';
+import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'app-crud-view',
-  imports: [],
+  imports: [ListScaperoomsComponent,RouterOutlet, CommonModule, ModalLayoutComponent,ModalComponent],
   templateUrl: './crud-view.component.html',
   styleUrl: './crud-view.component.scss'
 })
 export class CrudViewComponent {
+  isModalVisible = true;
 
+  openModal() {
+    console.log('🔵 openModal() ejecutado');
+    this.isModalVisible = true;
+    console.log('Estado de isModalVisible:', this.isModalVisible)
+  
+  }
+
+  closeModal() {
+    this.isModalVisible = false;
+  }
 }
