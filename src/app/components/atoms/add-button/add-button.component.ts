50b8c321b1917bf4d834b24631d458bfeaed36c2
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-20 11:31:57 +0100

    Add functionality to open modal in the add button component and create tests for the edit button component

--- src/app/components/atoms/add-button/add-button.component.ts
+++ src/app/components/atoms/add-button/add-button.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-add-button',
   imports: [],
-  template: `<button class="btn btn-primary">{{text}}</button>`,
+  template: `<button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" (click)="open()">{{text}}</button>`,
   //styleUrl: './add-button.component.scss'
 })
 export class AddButtonComponent {
   @Input() text: string = 'Add ScapeRoom';
+  @Output() openModal = new EventEmitter<void>();
+
+ open() {
+  console.log('🟢 Emitiendo evento para abrir modal');
+    this.openModal.emit();
+  } 
 }
