e86e4ca63c1c28ca8434f7b2df81a4121f48835c
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-20 11:33:39 +0100

    Remove the import of the ModalLayout component in CrudViewComponent

--- src/app/pages/crud-view/crud-view.component.ts
+++ src/app/pages/crud-view/crud-view.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { ListScaperoomsComponent } from '../../components/list-scaperooms/list-scaperooms.component';
 import { RouterOutlet } from '@angular/router';
-import { ModalLayoutComponent } from '../../Layouts/modal-layout/modal-layout.component';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from '../../components/modal/modal.component';
 
 @Component({
   selector: 'app-crud-view',
-  imports: [ListScaperoomsComponent,RouterOutlet, CommonModule, ModalLayoutComponent,ModalComponent],
+  imports: [ListScaperoomsComponent,RouterOutlet, CommonModule,ModalComponent],
   templateUrl: './crud-view.component.html',
   styleUrl: './crud-view.component.scss'
 })
