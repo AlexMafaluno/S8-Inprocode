813f41c21943c13ab7ad754d3726f07ec90837f6
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-20 12:51:25 +0100

    Update the modal component to improve accessibility and closing functionality

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-modal',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
