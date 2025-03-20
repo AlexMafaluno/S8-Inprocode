3da151d4eaf7a524e7804536b5ad348e9abf23fa
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-20 12:53:02 +0100

    Update the add and edit buttons to use routing links instead of static modals

--- src/app/components/atoms/add-button/add-button.component.ts
+++ src/app/components/atoms/add-button/add-button.component.ts
@@ -1,9 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-button',
-  imports: [],
-  template: `<button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop" (click)="open()">{{text}}</button>`,
+  imports: [RouterModule],
+  template: `<button type="button" [routerLink]="['/crud/add']"  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+  Add ScapeRoom
+</button>`,
   //styleUrl: './add-button.component.scss'
 })
 export class AddButtonComponent {
--- src/app/components/atoms/edit-button/edit-button.component.ts
+++ src/app/components/atoms/edit-button/edit-button.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-edit-button',
-  imports: [],
-  template: `<button class="btn btn-primary btn-sm">{{text}}</button>`,
+  imports: [RouterModule],
+  template: `<button type="button" [routerLink]="['/crud/edit', id]" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop"> Edit
+</button>`,
   styleUrl: './edit-button.component.scss'
 })
 export class EditButtonComponent {
+  @Input() id!: number;
 @Input() text: string = 'Edit ScapeRoom';
 }
