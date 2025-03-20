74e4f1b3abe2ea72e3f77bec54ba188d74870bf4
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-20 12:52:13 +0100

    Remove the static modal and simplify the visibility logic in the CrudView component

--- src/app/pages/crud-view/crud-view.component.ts
+++ src/app/pages/crud-view/crud-view.component.ts
@@ -11,16 +11,5 @@ import { ModalComponent } from '../../components/modal/modal.component';
   styleUrl: './crud-view.component.scss'
 })
 export class CrudViewComponent {
-  isModalVisible = true;
 
-  openModal() {
-    console.log('🔵 openModal() ejecutado');
-    this.isModalVisible = true;
-    console.log('Estado de isModalVisible:', this.isModalVisible)
-  
-  }
-
-  closeModal() {
-    this.isModalVisible = false;
-  }
 }
