ef9b5ea9318963aa2771a4a53e9e3b61b587f0be
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-21 14:09:36 +0100

    refactor(location.service): update API URL to use environment configuration

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LocationService {
-private apiUrl = 'http://localhost:3000/save-location';
-  constructor(private http: HttpClient) {}
+private myAppUrl: string;
+private myApiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.myAppUrl = environment.endpoint;
+    this.myApiUrl = 'save-location'
+  }
 
   addLocation(lat: number, lng: number):Observable<any>{
-    return this.http.post(this.apiUrl, { lat, lng });
+    return this.http.post((this.myAppUrl + this.myApiUrl), { lat, lng });
   }
 }
