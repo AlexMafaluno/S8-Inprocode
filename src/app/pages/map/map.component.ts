aca37e6cff374563a2e5a854ffd1c1806db677b0
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-21 13:04:28 +0100

    Add marker functionality to the map component and location service

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import * as L from 'leaflet'; // ✅ Importa Leaflet
+import { LocationService } from '../../services/location.service';
 
 @Component({
   selector: 'app-map',
@@ -10,23 +11,43 @@ import * as L from 'leaflet'; // ✅ Importa Leaflet
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements AfterViewInit {
-private map!: L.Map;
-ngAfterViewInit(): void {
-  this.initMap();
-}
+  private map!: L.Map;
+  marker!: L.Marker;
 
-private initMap(): void {
-  if (!document.getElementById('map')) {
-    console.error('❌ Error: The #map element does not exist.');
-    return;
+  private locationService = inject(LocationService);
+  ngAfterViewInit(): void {
+    this.initMap();
   }
-  this.map = L.map('map').setView([41.3851, 2.1734], 13); // ✅ Inicializa el mapa
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19,
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(this.map);
+  private initMap(): void {
+    if (!document.getElementById('map')) {
+      console.error('❌ Error: The #map element does not exist.');
+      return;
+    }
+    this.map = L.map('map').setView([41.3851, 2.1734], 13); // ✅ Inicializa el mapa
 
-L.marker([41.4667, 2.2803]).addTo(this.map);
-}
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.map);
+
+    L.marker([41.4667, 2.2803]).addTo(this.map);
+
+    this.map.on('click', (event: L.LeafletMouseEvent) => {
+      if (this.marker) {
+        this.marker.setLatLng(event.latlng);
+      } else {
+        this.marker = L.marker(event.latlng).addTo(this.map);
+      }
+      console.log('Ubicación:', event.latlng); // Se envía al backend
+    
+      this.locationService
+      .addLocation(event.latlng.lat, event.latlng.lng)
+      .subscribe((response) => console.log(response));
+    
+    });
+   
+    
+  };
 }
