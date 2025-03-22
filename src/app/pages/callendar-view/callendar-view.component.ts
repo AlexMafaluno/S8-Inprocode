c685e3514049fceea2b397fc9b7d72e06d61ff16
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-22 22:56:59 +0100

    Add CallendarViewComponent with template, styles, and tests

---
+++ src/app/pages/callendar-view/callendar-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FullcalendarComponent } from "../../components/fullcalendar/fullcalendar.component";
+
+@Component({
+  selector: 'app-callendar-view',
+  imports: [FullcalendarComponent],
+  templateUrl: './callendar-view.component.html',
+  styleUrl: './callendar-view.component.scss'
+})
+export class CallendarViewComponent {
+
+}
