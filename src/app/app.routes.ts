999e1777276e369965599ee031dbb0198dab5e0f
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-22 22:56:24 +0100

    Replace FullcalendarComponent with CallendarViewComponent in app routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MapComponent } from './pages/map/map.component';
-import { FullcalendarComponent } from './pages/fullcalendar/fullcalendar.component';
 import { ChartsComponent } from './pages/charts/charts.component';
 import { ListScaperoomsComponent } from './components/list-scaperooms/list-scaperooms.component';
 import { ScapeRoomCardComponent } from './components/scape-room-card/scape-room-card.component';
 import { CrudViewComponent } from './pages/crud-view/crud-view.component';
 import { ModalComponent } from './components/modal/modal.component';
+import { CallendarViewComponent } from './pages/callendar-view/callendar-view.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
   { path: 'map', component: MapComponent },
-  { path: 'callendar', component: FullcalendarComponent },
+  { path: 'callendar', component: CallendarViewComponent },
   { path: 'charts', component: ChartsComponent },
 
 
