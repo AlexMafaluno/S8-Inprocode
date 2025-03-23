865e715721eba5b6419b3034018a8e617f7f1985
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-23 21:44:46 +0100

    Add EventsService with methods for adding and deleting events

---
+++ src/app/services/events.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+private myAppUrl: string;
+private myApiUrl: string;
+  constructor(private http: HttpClient) {
+    this.myAppUrl= environment.endpoint;
+    this.myApiUrl= 'event/';
+  }
+  addEvent(name:string, description:string, date:string, time:string, people:number): Observable<any>{
+   return this.http.post((this.myAppUrl+ this.myApiUrl + "save-Event"),{name, description, date, time, people});
+   }
+
+   deleteEvent(id:number): Observable<any>{
+    return this.http.delete((this.myAppUrl + this.myApiUrl + id))
+   }
+}
