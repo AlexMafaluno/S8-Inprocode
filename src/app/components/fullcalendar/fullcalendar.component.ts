45ac30488b3e1d3d261b4376c0a6a860a6d0eb02
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-23 21:44:32 +0100

    Implement event handling and calendar instance verification in FullcalendarComponent

--- src/app/components/fullcalendar/fullcalendar.component.ts
+++ src/app/components/fullcalendar/fullcalendar.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FullCalendarModule } from '@fullcalendar/angular';
+import { AfterViewInit, Component, inject, ViewChild } from '@angular/core';
+import { FullCalendarComponent, FullCalendarModule } from '@fullcalendar/angular';
 import { CalendarOptions } from '@fullcalendar/core/index.js';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
+import { EventsService } from '../../services/events.service';
 
 @Component({
   selector: 'app-fullcalendar',
@@ -11,18 +12,64 @@ import interactionPlugin from '@fullcalendar/interaction';
   templateUrl: './fullcalendar.component.html',
   styleUrl: './fullcalendar.component.scss'
 })
-export class FullcalendarComponent {
+export class FullcalendarComponent implements AfterViewInit {
+  private eventsService =inject(EventsService);
+
+  @ViewChild('calendar') calendar: FullCalendarComponent | undefined; 
+
+  
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin, interactionPlugin],
     dateClick: (arg) => this.handleDateClick(arg),
-    events: [
-      {title: 'Hola', date:'2025-03-23'},
-      {title: 'pepe', date: '2025-02-22'}
-    ]
+    events: [{title:"pepe", date:"2025-03-24"}]
   };
 
+
+  ngAfterViewInit(): void {
+ // Verifica si la referencia calendar está disponible
+ if (this.calendar) {
+  console.log('FullCalendar instance is available');
+}
+}
+
   handleDateClick(arg:any) {
-    alert('date click!' + arg.dateStr);
+    const selectedDate = arg.dateStr;
+    alert('date click!' + selectedDate);
+
+    const newEvent = {
+      name: 'Nuevo evento',
+      description: 'Descripción del evento',
+      date: selectedDate,
+      time: '18:00:00',
+      people: 5
+    };
+
+    this.eventsService.addEvent(
+      newEvent.name,
+      newEvent.description,
+      newEvent.date,
+      newEvent.time,
+      newEvent.people
+    ).subscribe({
+      next: response => 
+        {console.log("evento agregado:", response);
+      alert("evento agregado con exito:");
+
+      if (Array.isArray(this.calendarOptions.events)) {
+        this.calendarOptions.events.push({
+          title: newEvent.name,
+          date: newEvent.date
+        });
+      }
+      if (this.calendar) {
+        this.calendar.getApi().refetchEvents();  // Actualiza los eventos en el calendario
+      }
+    },
+      error: err => {
+        console.error("Error al agregar evento:",err);
+        alert("Error al agregar el evento");
+      }
+    });
   }
 }
