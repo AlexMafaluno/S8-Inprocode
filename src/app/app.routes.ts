afe1e97b6d148c07ca124dc14e41f547e2f6e5a2
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-24 19:58:09 +0100

    fix(routes): update charts route to use ChartViewComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MapComponent } from './pages/map/map.component';
-import { ChartsComponent } from './pages/charts/charts.component';
+import { ChartsComponent } from './components/charts/charts.component';
 import { ListScaperoomsComponent } from './components/list-scaperooms/list-scaperooms.component';
 import { ScapeRoomCardComponent } from './components/scape-room-card/scape-room-card.component';
 import { CrudViewComponent } from './pages/crud-view/crud-view.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { CallendarViewComponent } from './pages/callendar-view/callendar-view.component';
+import { ChartViewComponent } from './pages/chart-view/chart-view.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
   { path: 'map', component: MapComponent },
   { path: 'callendar', component: CallendarViewComponent },
-  { path: 'charts', component: ChartsComponent },
+  { path: 'charts', component: ChartViewComponent },
 
 
   {
