02e7d6f908e4f6116df826cee548151078a10019
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-24 23:40:53 +0100

    Add loading spinner and handle empty state in ScapeRooms list component

--- src/app/components/list-scaperooms/list-scaperooms.component.ts
+++ src/app/components/list-scaperooms/list-scaperooms.component.ts
@@ -6,27 +6,33 @@ import { RouterModule } from '@angular/router';
 import { ScapeRoomCardComponent } from "../scape-room-card/scape-room-card.component";
 import { FilterComponent } from "../filter/filter.component";
 import { CardComponent } from "../card/card.component";
+import { ProgressSpinnerComponent } from "../progress-spinner/progress-spinner.component";
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-list-scaperooms',
-  imports: [CommonModule, RouterModule, FilterComponent, CardComponent],
+  imports: [CommonModule, RouterModule, FilterComponent, CardComponent, ProgressSpinnerComponent],
   templateUrl: './list-scaperooms.component.html',
   styleUrl: './list-scaperooms.component.scss',
 })
 export class ListScaperoomsComponent implements OnInit {
   listScapeRooms: ScapeRoomItem[] = [];
   private scaperoomService = inject(ScaperoomService);
-  
+  private toastr = inject(ToastrService);
+  loading: boolean = false;
+
  ngOnInit(){
 this.getlistScapeRooms();
   }
 
   getlistScapeRooms(){
+    this.loading = true;
     this.scaperoomService.getListScapeRooms().subscribe({
       next: (response) => {
         console.log('Respuesta de la API:', response); // 🔍 Para verificar los datos
         console.log('📢 Tipo de response:', typeof response);
         this.listScapeRooms = response.data || []; // ✅ Asigna directamente la respuesta
+        this.loading = false;
       },
       error: (error) => {
         console.error('Error al obtener escape rooms:', error);
@@ -38,10 +44,12 @@ this.getlistScapeRooms();
   deleteScapeRoom(id: number){ {
 
     this.scaperoomService.deleteScapeRoom(id).subscribe(() =>{
-      //arra loadig = true
+      this.loading = true;
       this.getlistScapeRooms();
-      //arra loadig = false;
+      this.loading = false;
+      this.toastr.warning('El producto fue eliminado con exito', 'Producto eliminado');
     })
   }
 }
+
 }
