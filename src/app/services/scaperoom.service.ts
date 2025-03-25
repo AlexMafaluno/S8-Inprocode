1660d02aa9bd8763133fa2268f9944efdb5761ff
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-25 12:09:53 +0100

    Add save and get methods for ScapeRoomItem in scaperoom service

--- src/app/services/scaperoom.service.ts
+++ src/app/services/scaperoom.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Scaperoom } from '../interfaces/scaperoom';
+import { Scaperoom, ScapeRoomItem } from '../interfaces/scaperoom';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +25,13 @@ private myApiUrl: string;
     return this.http.delete<void>(this.myAppUrl + this.myApiUrl + id);
   }
 
+  saveScapeRoom(scapeRoom :ScapeRoomItem): Observable<void>{
+   return this.http.post<void>(this.myAppUrl + this.myApiUrl, scapeRoom )
+  }
+
+  getScapeRoom(id: Number): Observable<ScapeRoomItem>{
+    return this.http.get<ScapeRoomItem>(this.myAppUrl + this.myApiUrl + id);
+  }
 /*async getListScapeRooms(): Promise<Scaperoom[]> {
   try {
     const response = await fetch((this.myAppUrl + this.myApiUrl), {
