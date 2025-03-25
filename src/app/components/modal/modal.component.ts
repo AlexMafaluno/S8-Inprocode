1311cdf187e945e82a33c1ae9aa437db287d7594
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-25 12:09:14 +0100

    Add loading state and dynamic title to modal component

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,37 +1,81 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Scaperoom, ScapeRoomItem } from '../../interfaces/scaperoom';
+import { ScaperoomService } from '../../services/scaperoom.service';
+import { ProgressSpinnerComponent } from "../progress-spinner/progress-spinner.component";
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-modal',
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, ProgressSpinnerComponent],
   templateUrl: './modal.component.html',
   styleUrl: './modal.component.scss'
 })
 export class ModalComponent implements OnInit{
  form: FormGroup;
- 
+ loading: boolean = false;
+ id : number = 0;
+ operacion:string = "add";
 
- constructor(private fb: FormBuilder){
+private scaperoomService = inject(ScaperoomService);
+private router = inject(Router);
+private toastr =  inject(ToastrService);
+private aRouter = inject(ActivatedRoute);
+private cdRef = inject(ChangeDetectorRef); // Inyectamos ChangeDetectorRef
+
+constructor(private fb: FormBuilder){
   this.form = this.fb.group({
     title: ['', Validators.required],
     director: ['', Validators.required]
   })
+    this.id = Number(this.aRouter.snapshot.paramMap.get('id'));
 }
+
+
   ngOnInit(): void {
-    //throw new Error('Method not implemented.');
+    
+    console.log('ID:', this.id);
+
+    if(this.id != 0){
+      this.operacion = "edit";
+      console.log('Operación cambiada a:', this.operacion);
+      this.getProduct(this.id);
+    }else {
+      this.operacion = "add";
+    }
   }
 
-
-  
 
 addProduct(){
   const scapeRoom: ScapeRoomItem = {
     title: this.form.value.title,
     director: this.form.value.director
   }
-  console.log(scapeRoom);
+  this.loading = true;
+  this.toastr.success(`El producto ${scapeRoom.title} fue registrado con exito`,`Producto registrado`);
+this.scaperoomService.saveScapeRoom(scapeRoom).subscribe(() => {
+  console.log("producto agregado");
+  this.loading = false; 
+  this.router.navigate(['/crud'])
+});
+}
+
+getProduct(id: Number){
+  this.loading =true;
+  this.scaperoomService.getScapeRoom(id).subscribe((response:any)=>{
+    console.log("respeusta del servidor:", response);
+    this.loading =false;
+    if (response.data) {
+    this.form.patchValue({
+      title: response.data.title,
+      director: response.data.director
+    });
+     // Forzar la detección de cambios para actualizar el formulario
+     this.cdRef.detectChanges();
+  }
+  
+  });
 }
 }
