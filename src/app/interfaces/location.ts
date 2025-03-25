7943786de915c7833e8fb66349d90be7200d55dc
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-25 13:59:40 +0100

    refactor(location): restructure Location interface to simplify data structure

--- src/app/interfaces/location.ts
+++ src/app/interfaces/location.ts
@@ -1,11 +1,9 @@
 export interface LocationItem {
     code:    number;
     message: string;
-    data:    Location[];
-}
-
-export interface Location {
-    idLocations?: number;
-    latitud:     string;
-    longitud:    string;
+    data:   [{
+        idLocations?: number;
+        latitud:     string;
+        longitud:    string;
+}];
 }
