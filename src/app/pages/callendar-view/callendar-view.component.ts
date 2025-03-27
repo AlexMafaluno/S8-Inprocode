c950b6012a4117eab08c36e59f189cdfe2268c25
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-27 15:27:05 +0100

    Add button to open modal for event creation in calendar view

--- src/app/pages/callendar-view/callendar-view.component.ts
+++ src/app/pages/callendar-view/callendar-view.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FullcalendarComponent } from "../../components/fullcalendar/fullcalendar.component";
+import { ModalEventComponent } from "../../components/modal-event/modal-event.component";
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-callendar-view',
-  imports: [FullcalendarComponent],
+  imports: [FullcalendarComponent, ModalEventComponent, RouterModule],
   templateUrl: './callendar-view.component.html',
   styleUrl: './callendar-view.component.scss'
 })
