c67fdb4acab76cdab01f58909d41cc1df7731b82
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-27 15:30:30 +0100

    Add CommonModule import to CallendarViewComponent for template functionality

--- src/app/pages/callendar-view/callendar-view.component.ts
+++ src/app/pages/callendar-view/callendar-view.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FullcalendarComponent } from "../../components/fullcalendar/fullcalendar.component";
 import { ModalEventComponent } from "../../components/modal-event/modal-event.component";
 import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-callendar-view',
-  imports: [FullcalendarComponent, ModalEventComponent, RouterModule],
+  imports: [FullcalendarComponent, ModalEventComponent,CommonModule, RouterModule],
   templateUrl: './callendar-view.component.html',
   styleUrl: './callendar-view.component.scss'
 })
