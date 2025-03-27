a401c420a392b6094779f97c12982689548ed1cb
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-27 14:31:10 +0100

    Refactor button components to use dynamic text input and remove unused modal event emitter

--- src/app/components/atoms/add-button/add-button.component.ts
+++ src/app/components/atoms/add-button/add-button.component.ts
@@ -5,16 +5,10 @@ import { RouterModule } from '@angular/router';
   selector: 'app-add-button',
   imports: [RouterModule],
   template: `<button type="button" [routerLink]="['/crud/add']"  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-  Add ScapeRoom
+  {{text}}
 </button>`,
   //styleUrl: './add-button.component.scss'
 })
 export class AddButtonComponent {
   @Input() text: string = 'Add ScapeRoom';
-  @Output() openModal = new EventEmitter<void>();
-
- open() {
-  console.log('🟢 Emitiendo evento para abrir modal');
-    this.openModal.emit();
-  } 
 }
