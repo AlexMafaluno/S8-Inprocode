406dfa68288ab47436d19b0a0688f9c6dba9bd06
Alejandro Martinez <mart.perez93@gmail.com>
2025-03-27 15:27:36 +0100

    Add route for ModalEventComponent under CallendarViewComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,17 @@ import { ScapeRoomCardComponent } from './components/scape-room-card/scape-room-
 import { CrudViewComponent } from './pages/crud-view/crud-view.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { CallendarViewComponent } from './pages/callendar-view/callendar-view.component';
+import { ModalEventComponent } from './components/modal-event/modal-event.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
   { path: 'map', component: MapComponent },
-  { path: 'callendar', component: CallendarViewComponent },
+  { path: 'callendar', component: CallendarViewComponent
+    ,children: [
+    { path: 'add', component: ModalEventComponent }
+  ] },
   { path: 'charts', component: ChartsComponent },
-
-
   {
     path: 'crud',
     component: CrudViewComponent,
