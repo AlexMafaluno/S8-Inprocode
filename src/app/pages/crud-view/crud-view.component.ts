84eb84761a4b5cf6948aa255595ff6cfb00add9b
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-05 09:14:53 +0200

    add filter button and modal filter components to CRUD view

--- src/app/pages/crud-view/crud-view.component.ts
+++ src/app/pages/crud-view/crud-view.component.ts
@@ -3,10 +3,12 @@ import { ListScaperoomsComponent } from '../../components/list-scaperooms/list-s
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from '../../components/modal/modal.component';
+import { FilterButtonComponent } from "../../components/atoms/filter-button/filter-button.component";
+import { ModalFilterComponent } from "../../components/modal-filter/modal-filter.component";
 
 @Component({
   selector: 'app-crud-view',
-  imports: [ListScaperoomsComponent, RouterOutlet, CommonModule, ModalComponent],
+  imports: [ListScaperoomsComponent,CommonModule, ModalComponent, FilterButtonComponent],
   templateUrl: './crud-view.component.html',
   styleUrl: './crud-view.component.scss'
 })
