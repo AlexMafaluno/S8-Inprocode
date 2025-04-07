c3faad57932a4fd69d15048ab00a78a10fb031b5
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-07 21:00:04 +0200

    feat: update routing to replace CrudViewComponent with ScaperoomsCollectionViewComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { MapComponent } from './pages/map/map.component';
 import { ChartsComponent } from './components/charts/charts.component';
 import { ListScaperoomsComponent } from './components/list-scaperooms/list-scaperooms.component';
 import { ScapeRoomCardComponent } from './components/scape-room-card/scape-room-card.component';
-import { CrudViewComponent } from './pages/crud-view/crud-view.component';
+import { ScaperoomsCollectionViewComponent } from './pages/scaperooms-collection-view/scaperooms-collection-view.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { CallendarViewComponent } from './pages/callendar-view/callendar-view.component';
 import { ChartViewComponent } from './pages/chart-view/chart-view.component';
@@ -29,7 +29,7 @@ export const routes: Routes = [
   { path: 'charts', component: ChartsComponent },
   {
     path: 'crud',
-    component: CrudViewComponent,
+    component: ScaperoomsCollectionViewComponent,
     children: [
       { path: 'add', component: ModalComponent },
       { path: 'edit/:id', component: ModalComponent } // 👈 Child route dentro de 'crud'
