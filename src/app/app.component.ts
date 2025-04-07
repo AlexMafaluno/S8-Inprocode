4813d9b020a7915074bbffa2fd29034f842124f2
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-07 11:58:26 +0200

    feat: replace header component with layout component for improved structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,14 @@ import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./Layouts/header/header.component";
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LayoutComponent } from "./Layouts/layout/layout.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [ LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  showNavbar= true;
-
-  private router = inject(Router);
-
-  constructor(){
-    this.router.events.subscribe(()=> {
-      this.showNavbar = !['/login', '/register'].includes(this.router.url);
-    });
-  }
 
 }
