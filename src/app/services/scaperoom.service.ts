7a5b606053940ba9292fab3c2bd61f2f285f3d1f
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-08 11:55:28 +0200

    refactor: update getListScapeRooms method to return an array of ScapeRoom and remove unused code

--- src/app/services/scaperoom.service.ts
+++ src/app/services/scaperoom.service.ts
@@ -10,15 +10,14 @@ import { ScapeRoom, ScapeRoomItem } from '../interfaces/scaperoom';
 export class ScaperoomService {
 private myAppUrl: string;
 private myApiUrl: string;
-//private listScapeRooms: Scaperoom = [];
 
   constructor(private http: HttpClient) {
     this.myAppUrl= environment.endpoint;
     this.myApiUrl= 'api/scaperoom/';
    }
 
-   getListScapeRooms(): Observable<ScapeRoomItem> {
-   return this.http.get<{ data: ScapeRoomItem }>(this.myAppUrl + this.myApiUrl).
+   getListScapeRooms(): Observable<ScapeRoom[]> {
+   return this.http.get<{ data: ScapeRoom []}>(this.myAppUrl + this.myApiUrl).
    pipe(
      map(response => response.data)
    )
@@ -42,32 +41,5 @@ private myApiUrl: string;
   updateScapeRoom(id:number, scapeRoom: ScapeRoom): Observable<void>{
     return this.http.patch<void>((this.myAppUrl + this.myApiUrl + id),scapeRoom);
   }
-/*async getListScapeRooms(): Promise<Scaperoom[]> {
-  try {
-    const response = await fetch((this.myAppUrl + this.myApiUrl), {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-      },
-    });
 
-    if (!response.ok) {
-      console.warn("Error en la respuesta de l'API");
-      return this.listScapeRooms;
-    }
-
-    const data = await response.json();
-
-    if (!data.data || data.data.length === 0) {
-      console.warn('No se encontraron más naves.');
-      return this.listScapeRooms;
-    }
-
-    return this.listScapeRooms;
-  } catch (error) {
-    console.error('Ha habido un error:', error);
-    throw error;
-  }
-}
-*/
 }
