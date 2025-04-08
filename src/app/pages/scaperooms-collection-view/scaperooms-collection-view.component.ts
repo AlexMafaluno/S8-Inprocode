d8bad39e01f480258793baa4e329339a4afc2e01
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-08 11:56:48 +0200

    feat: integrate FilterComponent into ScaperoomsCollectionView for enhanced filtering options

--- src/app/pages/scaperooms-collection-view/scaperooms-collection-view.component.ts
+++ src/app/pages/scaperooms-collection-view/scaperooms-collection-view.component.ts
@@ -3,10 +3,11 @@ import { ListScaperoomsComponent } from '../../components/list-scaperooms/list-s
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from '../../components/modal/modal.component';
+import { FilterComponent } from "../../components/filter/filter.component";
 
 @Component({
   selector: 'app-scaperooms-collection-view',
-  imports: [ListScaperoomsComponent, CommonModule, ModalComponent],
+  imports: [ListScaperoomsComponent, CommonModule, ModalComponent, FilterComponent],
   templateUrl: './scaperooms-collection-view.component.html',
   styleUrl: './scaperooms-collection-view.component.scss'
 })
