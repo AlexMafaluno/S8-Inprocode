adfcee37e097fd8e7b133321b660e8a4b676ab3f
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-08 11:56:23 +0200

    feat: update CardDetailPage to include CardComponent and fetch selectedCard with ScaperoomFacadeService

--- src/app/pages/card-detail-page/card-detail-page.component.ts
+++ src/app/pages/card-detail-page/card-detail-page.component.ts
@@ -3,10 +3,12 @@ import { ScapeRoomCardComponent } from "../../components/scape-room-card/scape-r
 import { ScaperoomService } from '../../services/scaperoom.service';
 import { ActivatedRoute } from '@angular/router';
 import { ScapeRoom } from '../../interfaces/scaperoom';
+import { CardComponent } from "../../components/card/card.component";
+import { ScaperoomFacadeService } from '../../services/scaperoom-facade.service';
 
 @Component({
   selector: 'app-card-detail-page',
-  imports: [ScapeRoomCardComponent],
+  imports: [ScapeRoomCardComponent, CardComponent],
   templateUrl: './card-detail-page.component.html',
   styleUrl: './card-detail-page.component.scss'
 })
@@ -16,13 +18,26 @@ selectedCard!: ScapeRoom;
 
 private route = inject(ActivatedRoute);
 private scaperoomService = inject(ScaperoomService);
+private scaperoomFacadeService = inject(ScaperoomFacadeService)
 constructor(){}
 ngOnInit(): void {
   const id = Number(this.route.snapshot.paramMap.get('id'));
-  console.log(id);
+  const userId = 359; 
+  
+  this.scaperoomFacadeService.getScapeRoomWithPotos(userId).subscribe((scapeRooms) => {
+    const scaperoom = scapeRooms.find(room => room.id === id);
+    if (scaperoom) {
+      this.selectedCard = scaperoom;
+      console.log("ScapeRoom con foto:", this.selectedCard);
+    } else {
+      console.warn(`No se encontró un scaperoom con id ${id}`);
+    }
+  });
+  /*
   this.scaperoomService.getScapeRoom(id).subscribe((scaperoom) => {
     console.log("ScapeRoom recibido:", scaperoom);
     this.selectedCard = scaperoom;
   })
+    */
 }
 }
