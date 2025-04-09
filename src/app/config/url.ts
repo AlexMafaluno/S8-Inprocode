5c5cb9c75abb294144d38a33c45aa0e12c54f3d1
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-09 11:46:23 +0200

    fix: refactor API endpoint configuration for improved structure and readability

--- src/app/config/url.ts
+++ src/app/config/url.ts
@@ -1,21 +1,30 @@
-export const url = {
-    // API_URL: 'https://api.example.com', // Replace with your actual API URL
+export const API_ROOT= 'http://localhost:3000';
 
-    //ROOT_URL:
-    API_URL: 'http://localhost:3000',
+export const API_ENDPOINTS = {
+  
+    SCAPEROOM:{
+        BASE: `${API_ROOT}/api/scaperoom/`,
+    },
+    LOCATION: {
+        BASE: `${API_ROOT}/location/`,
+        SAVE: `${API_ROOT}/location/save-location`,
+        BY_GENRE: `${API_ROOT}/location?genre=`
+    },
+    EVENT: {
+        BASE: `${API_ROOT}/event/`,
+        SAVE: `${API_ROOT}/event/save-Event`,
+    },
+    PHOTO: {
+        BASE: `${API_ROOT}/photo/`,
+        USER: `${API_ROOT}/photos/user/`,
+        UPLOAD: `${API_ROOT}/upload-photo`,
+    },
 
-    //SCAPEROOM_URL:
-    API_PATH: '/api/scaperoom/' ,
 
-    //LOCATION_URL:
-    API_LOCATION: 'location/',
-
-    //EVENT_URL:
-    API_EVENT: 'event/',
-
-    //PHOTO_URL:
-    PHOTO: 'photo/',
-    PHOTO_USER:'photos/user/',
-    PHOTO_UPLOAD:'upload-photo',
-
+    // Dinámicas
+  getScaperoomById: (id: number) => `${API_ENDPOINTS.SCAPEROOM.BASE}${id}`,
+  deleteScaperoomById: (id: number) => `${API_ENDPOINTS.SCAPEROOM.BASE}${id}`,
+  updateScaperoomById: (id: number) => `${API_ENDPOINTS.SCAPEROOM.BASE}${id}`,
+  getPhotoByUserId: (userId: string) => `${API_ROOT}/api/photos/user/${userId}`,
+  deleteEventById: (id: number) => `${API_ENDPOINTS.EVENT.BASE}${id}`,
 }
