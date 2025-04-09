551fa69a6a35002c72587567aea4443f267f6bb8
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-09 12:08:16 +0200

    fix: refactor photo and upload image services to use API_ENDPOINTS for consistency

--- src/app/services/upload-image.service.ts
+++ src/app/services/upload-image.service.ts
@@ -2,26 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
-
+import { API_ENDPOINTS } from '../config/url';
 @Injectable({
   providedIn: 'root'
 })
 export class UploadImageService {
-  private myAppUrl: string;
-  private myApiUrl: string;
-  constructor(private http: HttpClient) {
-    this.myAppUrl = environment.endpoint;
-    this.myApiUrl = 'api/scaperoom/upload-photo';
-   }
+
+  constructor(private http: HttpClient) {}
 
 uploadImage(file: File, scaperoomId:number ): Observable<any>{
   const formData = new FormData();
   formData.append('file', file);
   formData.append('id', scaperoomId.toString());
 
-  return this.http.post<any>((this.myAppUrl + this.myApiUrl),formData);
-
-  
-}
+  return this.http.post<any>(API_ENDPOINTS.PHOTO.UPLOAD,formData);}
 
 }
