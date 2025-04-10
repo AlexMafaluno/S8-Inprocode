9d0704c9182eea5024e53e881f3011acb3b5a989
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-10 16:23:47 +0200

    feat: implement AdminCrudPageComponent with HTML template, styles, and unit tests

---
+++ src/app/pages/admin-crud-page/admin-crud-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ListCrudComponent } from "../../components/list-crud/list-crud.component";
+import { ScapeRoom } from '../../interfaces/scaperoom';
+import { RouterModule } from '@angular/router';
+import { ScaperoomService } from '../../services/scaperoom.service';
+
+@Component({
+  selector: 'app-admin-crud-page',
+  imports: [ListCrudComponent,RouterModule],
+  templateUrl: './admin-crud-page.component.html',
+  styleUrl: './admin-crud-page.component.scss'
+})
+export class AdminCrudPageComponent implements OnInit {
+
+
+  listScapeRooms :ScapeRoom[] = [];
+   private scapeRoomService= inject(ScaperoomService)
+  
+  loadScapeRooms() {
+    this.scapeRoomService.getListScapeRooms().subscribe((data) => {
+      this.listScapeRooms = data;
+      console.log("Data:", data);
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadScapeRooms();
+  }
+
+  formUpdated(updatedRoom: ScapeRoom) {
+  console.log('Formulario actualizado desde el hijo:', updatedRoom);
+  this.loadScapeRooms(); // Recargar la lista después de actualizar
+    }
+}
