e4aa20d948686ab66a224d4bb66101ddd0e09b84
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-11 20:39:47 +0200

    feat: enhance scaperooms collection view with loading state and data fetching

--- src/app/pages/scaperooms-collection-view/scaperooms-collection-view.component.ts
+++ src/app/pages/scaperooms-collection-view/scaperooms-collection-view.component.ts
@@ -1,16 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ListScaperoomsComponent } from '../../components/list-scaperooms/list-scaperooms.component';
-import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from '../../components/modal/modal.component';
 import { FilterComponent } from "../../components/filter/filter.component";
+import { ScapeRoom } from '../../interfaces/scaperoom';
+import { Photo } from '../../interfaces/photo';
+import { ScaperoomFacadeService } from '../../services/scaperoom-facade.service';
+import { ProgressSpinnerComponent } from "../../components/progress-spinner/progress-spinner.component";
 
 @Component({
   selector: 'app-scaperooms-collection-view',
-  imports: [ListScaperoomsComponent, CommonModule, ModalComponent, FilterComponent],
+  imports: [ListScaperoomsComponent, CommonModule, ModalComponent, FilterComponent, ProgressSpinnerComponent],
   templateUrl: './scaperooms-collection-view.component.html',
   styleUrl: './scaperooms-collection-view.component.scss'
 })
-export class ScaperoomsCollectionViewComponent {
+export class ScaperoomsCollectionViewComponent implements OnInit {
+  listScapeRooms: ScapeRoom[] = [];
+  userPhotos: Photo[] = [];
+  loading: boolean = false;
+  
+  private scaperoomFacade = inject(ScaperoomFacadeService);
+  
+  
 
+loadScapeRooms(userId: number): void {
+    this.loading = true;
+    this.scaperoomFacade.getScapeRoomWithPotos(userId).subscribe({
+      next: (scapeRooms) => {
+        this.listScapeRooms = scapeRooms;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error al cargar los scaperooms con fotos:', err);
+        this.listScapeRooms = [];
+        this.loading = false;
+      }
+    });
+  }
+  ngOnInit(): void {
+    this.loadScapeRooms(359); // Cambia el ID según sea necesario
+  }
 }
