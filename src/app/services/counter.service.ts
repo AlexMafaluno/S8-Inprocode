278260c2e84d9e94ff2d5e87ee60c27a71bfe811
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-12 20:58:14 +0200

    feat: add computed level property and effect for level change detection in CounterService

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,16 +1,24 @@
-import { Injectable, signal } from '@angular/core';
+import { computed, effect, Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
 
-  private photoCount = signal<number>(0);
+  private photoCount = signal<number>(15);
+// Nueva señal: detecta nivel actual
+  level = computed(() => this.checkLevel(this.photoCount()));
 
   get count() {
     return this.photoCount.asReadonly(); // acceso solo lectura desde fuera
   }
-
+  constructor() {
+    // Este effect se dispara cada vez que cambia el nivel
+    effect(() => {
+      const newLevel = this.level();
+      this.onLevelChanged(newLevel);
+    });
+  }
   setCount(newCount: number) {
     this.photoCount.set(newCount);
   }
@@ -22,5 +30,30 @@ export class CounterService {
 
   reset() {
     this.photoCount.set(0);
+  }
+
+
+  checkLevel(value: number): string {
+    if (value <= 10) {
+      return 'Candado de madera';
+    } else if (value <= 25) {
+      return 'Candado de bronce';
+    }else if (value <= 50) {
+      return 'Candado de hierro';
+    } else if (value <= 75) {
+      return 'Candado de plata';
+    }else if (value < 100) {
+      return 'Candado de oro';
+    }else if (value < 150) {
+      return  'Candado de cristal';
+    } else {
+      return 'Candado místico';	
+    }
+  }
+
+ private onLevelChanged(level: string) {
+  // Aquí puedes disparar efectos: sonidos, animaciones, logs, etc.
+    console.log('Nivel cambiado a:', level);
+    // Ejemplo: lanzar una animación o cambiar un estado global
   }
 }
