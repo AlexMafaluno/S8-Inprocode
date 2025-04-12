0b3ad86a27ca6daabe22980eb0fcd475b5e072fa
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-12 20:57:49 +0200

    feat: add LevelStatusComponent to FilterComponent for enhanced functionality

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AddButtonComponent } from "../atoms/add-button/add-button.component";
 import { CounterComponent } from "../counter/counter.component";
+import { LevelStatusComponent } from "../atoms/level-status/level-status.component";
 
 @Component({
   selector: 'app-filter',
-  imports: [AddButtonComponent, CounterComponent],
+  imports: [CounterComponent, LevelStatusComponent],
   templateUrl: './filter.component.html',
   styleUrl: './filter.component.scss'
 })
