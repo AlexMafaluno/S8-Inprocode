6336ffa39e22883acb1df313ab853db3190e3e7b
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-12 16:20:09 +0200

    refactor: update ScapeRoomItem interface to use array for data and improve structure

--- src/app/interfaces/scaperoom.ts
+++ src/app/interfaces/scaperoom.ts
@@ -1,17 +1,23 @@
 export interface ScapeRoom {
-    id?: number;
-    title: string;
-    director: string;
-    poster?: string;
-    genre?: string;
-    imageUrl?: string;
+  id?: number;
+  title: string;
+  director: string;
+  poster?: string;
+  genre?: string;
+  imageUrl?: string;
 }
 
 export interface ScapeRoomItem {
-    map(arg0: (scaperoom: any) => any): any;
-    code: number;
-    message: string;
-    data: ScapeRoom; // ✅ Ahora `data` es un array de objetos dinámico
+  map(arg0: (scaperoom: any) => any): any;
+
+  pagination: {
+    currentPage: number;
+    totalItems: number;
+    totalPages: number;
+    perPage: number;
+    hasMore: boolean;
+  };
+  code?: number;
+  message?: string;
+  data: ScapeRoom[]; // ✅ Ahora `data` es un array de objetos dinámico
 }
-
-
