a7a76ef792b93b0a95fde391ab23743b6dc47f4d
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-14 12:25:03 +0200

    feat: enhance ListScaperoomsComponent to display achievement cards for specific item types

--- src/app/components/list-scaperooms/list-scaperooms.component.ts
+++ src/app/components/list-scaperooms/list-scaperooms.component.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CardComponent } from "../card/card.component";
 import { Photo } from '../../interfaces/photo';
+import { AchivementCardComponent } from "../achivement-card/achivement-card.component";
 
 @Component({
   selector: 'app-list-scaperooms',
-  imports: [CommonModule, RouterModule, CardComponent],
+  imports: [CommonModule, RouterModule, CardComponent, AchivementCardComponent],
   templateUrl: './list-scaperooms.component.html',
   styleUrl: './list-scaperooms.component.scss',
 })
