504467cdc49fe7e37e211f0ee408b2cd22c79314
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-16 09:26:02 +0200

    feat: enhance CounterService with toastr notifications and improve photo count management

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -1,26 +1,55 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
+private toastrService = inject(ToastrService);
+  private readonly storageKey = 'user-photo-count';
+  private photoCount = signal<number>(this.getStoredCount());
+  private counterThresholds = [10, 25, 50, 75, 99, 149]; // niveles 1, 2, 3, 4, etc.
+  private getStoredCount():number{
+    const stored = localStorage.getItem(this.storageKey);
+    return stored ? +stored : 0;
+  }
 
-  private photoCount = signal<number>(0);
 
   get count() {
     return this.photoCount.asReadonly(); // acceso solo lectura desde fuera
   }
 
   setCount(newCount: number) {
+    localStorage.setItem(this.storageKey,newCount.toString());
     this.photoCount.set(newCount);
   }
 
   increment(value: number) {
-    console.log('Incrementando contador...');
-    this.photoCount.update(count => count + value);
+    const newValue = this.photoCount() + value;
+    localStorage.setItem(this.storageKey, newValue.toString());
+    this.photoCount.set(newValue);
+    this.updateCount();
+  }
+
+  private updateCount() {
+    const xp = this.photoCount();
+    let newCount = 0;
+  
+    let lastThreshold = 0;
+    for (let i = 0; i < this.counterThresholds.length; i++) {
+      if (xp >= this.counterThresholds[i]) {
+        newCount = i + 1;
+        lastThreshold = this.counterThresholds[i];
+      }
+    }
+    this.toastrService.success(`Alcanzaste un total de ${lastThreshold} scapeRooms`, 'Exito')
+    this.photoCount.set(newCount);
   }
 
   reset() {
+    localStorage.removeItem(this.storageKey);
     this.photoCount.set(0);
   }
+
+
 }
