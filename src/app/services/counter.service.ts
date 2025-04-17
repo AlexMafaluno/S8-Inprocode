d4256af87a2a4696e7e6c690dacf18a664be93e7
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-17 08:59:15 +0200

    feat: enhance CounterService to manage user divisions and notifications on upgrades

--- src/app/services/counter.service.ts
+++ src/app/services/counter.service.ts
@@ -8,8 +8,43 @@ export class CounterService {
 private toastrService = inject(ToastrService);
   private readonly storageKey = 'user-photo-count';
   private photoCount = signal<number>(this.getStoredCount());
-  private counterThresholds = [8, 25, 50, 75, 99, 149]; // niveles 1, 2, 3, 4, etc.
+  private counterThresholds = [10, 25, 50, 75, 99, 149]; // divisones:
   private lastThresholdReached = 0;
+  private currentDivisionId = 0;
+
+  divisions: {
+    id: number;
+    name: string;
+    icon: string; // puede ser un emoji o ruta de imagen
+    threshold: number;
+  }[] = [
+    { id: 1, name: 'Candado de madera', icon: '🔒', threshold: 10 },
+    { id: 2, name: 'Candado de bronce', icon: '🟤', threshold: 25 },
+    { id: 3, name: 'Candado de plata', icon: '⚪', threshold: 50 },
+    { id: 4, name: 'Candado de oro', icon: '🟡', threshold: 75 },
+    { id: 5, name: 'Candado de diamante', icon: '💎', threshold: 99 },
+    { id: 6, name: 'Candado legendario', icon: '🔥', threshold: 149 }
+  ]
+
+  getCurrentDivision(roomsJugados: number) {
+    return this.divisions
+      .filter(div => roomsJugados >= div.threshold)
+      .sort((a, b) => b.threshold - a.threshold)[0]; // devuelve la más alta alcanzada
+  }
+
+  checkForDivisionUpgrade(roomsJugados: number) {
+    const newDivision = this.getCurrentDivision(roomsJugados);
+    if (newDivision && newDivision.id !== this.currentDivisionId) {
+      this.currentDivisionId = newDivision.id;
+      this.toastrService.success(
+        `¡Ascendiste a ${newDivision.name}!`,
+        `${newDivision.icon} Nueva división`
+      );
+    }
+  }
+  
+
+
   private getStoredCount():number{
     const stored = localStorage.getItem(this.storageKey);
     return stored ? +stored : 0;
@@ -30,6 +65,7 @@ private toastrService = inject(ToastrService);
     localStorage.setItem(this.storageKey, newValue.toString());
     this.photoCount.set(newValue);
     this.updateCount();
+    this.checkForDivisionUpgrade(newValue);
   }
 
   private updateCount() {
@@ -47,6 +83,7 @@ private toastrService = inject(ToastrService);
   if (lastThreshold > this.lastThresholdReached) {
     this.toastrService.success(`Alcanzaste un total de ${lastThreshold} scapeRooms`, 'Nuevo logro');
     this.lastThresholdReached = lastThreshold;
+    console.log(this.lastThresholdReached);
   }
   this.photoCount.set(newCount);
 }
@@ -55,6 +92,7 @@ private toastrService = inject(ToastrService);
     localStorage.removeItem(this.storageKey);
     this.photoCount.set(0);
   }
+
 
 
 }
