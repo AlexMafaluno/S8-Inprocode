2d358678125465f1cb8387aae68917b72e71e1cc
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-17 10:03:08 +0200

    feat: add UserBadge component with HTML, CSS, SCSS, and unit tests

---
+++ src/app/components/organisms/user-badge/user-badge.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { LevelComponent } from "../../atoms/level/level.component";
+import { RouterModule } from '@angular/router';
+import { AvatarPicComponent } from "../../atoms/avatar-pic/avatar-pic.component";
+import { ProgressBarComponent } from "../../atoms/progress-bar/progress-bar.component";
+
+@Component({
+  selector: 'app-user-badge',
+  imports: [LevelComponent, RouterModule, AvatarPicComponent, ProgressBarComponent],
+  templateUrl: './user-badge.component.html',
+  styleUrl: './user-badge.component.scss'
+})
+export class UserBadgeComponent {
+
+}
