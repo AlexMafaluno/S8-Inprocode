4ecdad34b12856b415e3b0be198e41a9b1dd86c2
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-19 10:15:38 +0200

    refactor: update sortedBy method to accept ScapeRoom type and remove unused sorting cases

--- src/app/services/filter.service.ts
+++ src/app/services/filter.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ScapeRoom } from '../interfaces/scaperoom';
 import { Observable } from 'rxjs';
+import { Photo } from '../interfaces/photo';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,13 @@ export class FilterService {
 
   constructor() { }
 
-  sortedBy(criteria: string, data: any[]) {
+  sortedBy(criteria: string, data: ScapeRoom[]) {
    
     switch (criteria) {
-      case 'name':
-        return data.sort((a, b) => a.title.localeCompare(b.title));
-      case 'tipo':
-        return data.sort((a, b) => a.price - b.price);
-      case 'fecha':
-        return data.sort((a, b) => a.capacity - b.capacity);
+      case 'id':
+        return data.sort((a, b) => a.id - b.id);
+      // case 'fecha':
+      //   return data.sort((a, b) => new Date(a.played_at).getTime() - new Date(b.played_at).getTime());
       default:
         return data;
     }
