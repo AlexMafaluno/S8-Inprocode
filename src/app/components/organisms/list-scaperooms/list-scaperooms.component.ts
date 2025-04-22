3e11f37ce22c5677b9219332d9a3472671eb9989
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-22 07:41:23 +0200

    refactor: replace AchivementCard component with AchivementItem component in list-scaperooms

--- src/app/components/organisms/list-scaperooms/list-scaperooms.component.ts
+++ src/app/components/organisms/list-scaperooms/list-scaperooms.component.ts
@@ -4,13 +4,13 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ScaperoomItemComponent } from "../../molecules/scaperoom-item/scaperoom-item.component";
 import { Photo } from '../../../interfaces/photo';
-import { AchivementCardComponent } from "../../achivement-card/achivement-card.component";
+import { AchivementItemComponent } from "../../achivement-item/achivement-item.component";
 import { ModalFilterComponent } from "../../modal-filter/modal-filter.component";
 import { FilterService } from '../../../services/filter.service';
 
 @Component({
   selector: 'app-list-scaperooms',
-  imports: [CommonModule, RouterModule, ScaperoomItemComponent, AchivementCardComponent],
+  imports: [CommonModule, RouterModule, ScaperoomItemComponent, AchivementItemComponent],
   templateUrl: './list-scaperooms.component.html',
   styleUrl: './list-scaperooms.component.scss',
 })
