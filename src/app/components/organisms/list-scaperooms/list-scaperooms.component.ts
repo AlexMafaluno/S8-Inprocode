a26ac5d2c398da5238ccfe13a5d6922219350aa4
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-22 07:42:54 +0200

    fix: correct import path for AchivementItemComponent in list-scaperooms

--- src/app/components/organisms/list-scaperooms/list-scaperooms.component.ts
+++ src/app/components/organisms/list-scaperooms/list-scaperooms.component.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ScaperoomItemComponent } from "../../molecules/scaperoom-item/scaperoom-item.component";
 import { Photo } from '../../../interfaces/photo';
-import { AchivementItemComponent } from "../../achivement-item/achivement-item.component";
+import { AchivementItemComponent } from "../../molecules/achivement-item/achivement-item.component";
 import { ModalFilterComponent } from "../../modal-filter/modal-filter.component";
 import { FilterService } from '../../../services/filter.service';
 
