e314e876bab03d2ace9d51dc03943af3ebb8df20
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-22 08:04:27 +0200

    fix: update import path for DropdownButtomComponent to reflect new directory structure

--- src/app/components/organisms/filter-panel/filter-panel.component.ts
+++ src/app/components/organisms/filter-panel/filter-panel.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { AddButtonComponent } from "../../atoms/add-button/add-button.component";
 import { CounterComponent } from "../../counter/counter.component";
-import { DropdownButtomComponent } from "../../dropdown-buttom/dropdown-buttom.component";
+import { DropdownButtomComponent } from "../../molecules/dropdown-buttom/dropdown-buttom.component";
 import { ScaperoomService } from '../../../services/scaperoom.service';
 import { ScapeRoom } from '../../../interfaces/scaperoom';
 
