be6862e5aa23fe96f0285c96ca9486d5e209f9a3
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-22 07:03:24 +0200

    feat: replace card component with scaperoom item component in list scaperooms

--- src/app/components/list-scaperooms/list-scaperooms.component.ts
+++ src/app/components/list-scaperooms/list-scaperooms.component.ts
@@ -2,7 +2,7 @@ import { Component,inject,Input, OnInit } from '@angular/core';
 import { ScapeRoom } from '../../interfaces/scaperoom';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { CardComponent } from "../card/card.component";
+import { ScaperoomItemComponent } from "../molecules/scaperoom-item/scaperoom-item.component";
 import { Photo } from '../../interfaces/photo';
 import { AchivementCardComponent } from "../achivement-card/achivement-card.component";
 import { ModalFilterComponent } from "../modal-filter/modal-filter.component";
@@ -10,7 +10,7 @@ import { FilterService } from '../../services/filter.service';
 
 @Component({
   selector: 'app-list-scaperooms',
-  imports: [CommonModule, RouterModule, CardComponent, AchivementCardComponent],
+  imports: [CommonModule, RouterModule, ScaperoomItemComponent, AchivementCardComponent],
   templateUrl: './list-scaperooms.component.html',
   styleUrl: './list-scaperooms.component.scss',
 })
