46a162f62ba435da47aefa9c94128055751cd6bd
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-23 17:02:58 +0200

    feat: enhance scaperooms collection page with modal components and error handling

--- src/app/pages/scaperooms-collection-page/scaperooms-collection-page.component.ts
+++ src/app/pages/scaperooms-collection-page/scaperooms-collection-page.component.ts
@@ -14,10 +14,12 @@ import { UserStatsComponent } from "../../components/organisms/user-stats/user-s
 import { FilterButtonComponent } from "../../components/atoms/filter-button/filter-button.component";
 import { ModalFilterComponent } from "../../components/organisms/modal-filter/modal-filter.component";
 import { FilterService } from '../../services/filter.service';
+import { ModalButtonComponent } from "../../components/atoms/modal-button/modal-button.component";
+import { DivisionModalComponent } from "../../components/organisms/division-modal/division-modal.component";
 
 @Component({
   selector: 'app-scaperooms-collection-page',
-  imports: [ListScaperoomsComponent, CommonModule,FilterPanelComponent, InfiniteScrollDirective, ProgressSpinnerComponent, UserStatsComponent, FilterButtonComponent, ModalFilterComponent],
+  imports: [ListScaperoomsComponent, CommonModule, FilterPanelComponent, InfiniteScrollDirective, ProgressSpinnerComponent, UserStatsComponent, FilterButtonComponent, ModalFilterComponent, ModalButtonComponent, DivisionModalComponent],
   templateUrl: './scaperooms-collection-page.component.html',
   styleUrl: './scaperooms-collection-page.component.scss'
 })
@@ -50,16 +52,14 @@ loadScapeRooms(userId: number): void {
   if(this.loading || !this.hasMore) return;
 
     this.loading = true;
-    this.scaperoomFacade.getScapeRoomWithPotos(userId, this.page)
-    // .pipe(
-    //   catchError((errorMessage) => {
-    //     console.log('Error desde el componente padre');
-    //     this.errorMessage = errorMessage;
-    //     this.loading = false;
-    //     return EMPTY;
-    //     // return throwError(() => errorMessage);
-    //   }))
+    this.scaperoomFacade.getScapeRoomWithPotos(userId, this.page).pipe(
+      catchError((err) => {
+        this.errorMessage = err.message;
+        this.loading = false; 
+        return EMPTY;
+      }))
       .subscribe({
+      
       next: (res: ScapeRoom[]) => {
         this.scaperoomFacade.addScapeRooms(res);
         this.listScapeRooms = this.scaperoomFacade.scapeRooms();
@@ -67,12 +67,6 @@ loadScapeRooms(userId: number): void {
         this.hasMore = res.length > 0; // Adjust logic if pagination is needed
         this.page++;
         console.log('Cargando página:', this.listScapeRooms);
-        this.loading = false;
-      },
-      error: (err) => {
-        console.error('Error al cargar los scaperooms con fotos:', err);
-        this.listScapeRooms = [];
-        this.errorMessage = err;
         this.loading = false;
       }
     });
