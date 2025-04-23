6b4dd5bbf0783d4554e1f58b177b91614a8bc1ad
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-23 13:24:49 +0200

    feat: update callendar page to include exit button component and fix import path for modal event component

--- src/app/pages/callendar-page/callendar-page.component.ts
+++ src/app/pages/callendar-page/callendar-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FullcalendarComponent } from "../../components/fullcalendar/fullcalendar.component";
-import { ModalEventComponent } from "../../components/modal-event/modal-event.component";
+import { ModalEventComponent } from "../../components/organisms/modal-event/modal-event.component";
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { ExitButtonComponent } from "../../components/atoms/exit-button/exit-button.component";
 
 @Component({
   selector: 'app-callendar-page',
-  imports: [FullcalendarComponent,CommonModule, RouterModule],
+  imports: [FullcalendarComponent, CommonModule, RouterModule, ExitButtonComponent],
   templateUrl: './callendar-page.component.html',
   styleUrl: './callendar-page.component.scss'
 })
