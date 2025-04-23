0fb5070fe6217c87cc110a8d0a73339a0e49918c
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-23 02:06:22 +0200

    feat: integrate file upload component and clean up image upload logic in scaperoom detail

--- src/app/components/organisms/scaperoom-detail/scaperoom-detail.component.ts
+++ src/app/components/organisms/scaperoom-detail/scaperoom-detail.component.ts
@@ -14,9 +14,11 @@ import { CounterService } from '../../../services/counter.service';
 import { PhotoService } from '../../../services/photo.service';
 import { UploadImageService } from '../../../services/upload-image.service';
 import { ScapeRoom, ScapeRoomItem } from '../../../interfaces/scaperoom';
+import { FileInputComponent } from "../../atoms/file-input/file-input.component";
+import { FileUploadComponent } from "../../molecules/file-upload/file-upload.component";
 @Component({
   selector: 'app-scaperoom-detail',
-  imports: [ExitButtonComponent, RouterModule],
+  imports: [ExitButtonComponent, RouterModule, FileUploadComponent],
   templateUrl: './scaperoom-detail.component.html',
   styleUrl: './scaperoom-detail.component.scss'
 })
@@ -27,68 +29,18 @@ export class ScaperoomDetailComponent implements OnInit{
   @Input() scapeRoom:ScapeRoomItem[] = [];
   userPhotos = signal<Photo[]>([]);
   
-
-  private uploadImageService = inject(UploadImageService);
-  private photoService = inject(PhotoService);
   private counterService = inject(CounterService);
-  private toastService = inject(NotificationService);
-  private router = inject(Router);
-  private aRouter = inject(ActivatedRoute);
-   private levelService = inject(LevelService);
-
+ 
 
 ngOnInit(): void {
   // this.increaseCounter();
 }
 
-
-  onUpload(arg0: number) {
-    if (!this.selectedFile) {
-      console.error('No se ha seleccionado un archivo.');
-      return;
-    }
-    console.log(this.card);
-    this.photoService.uploadImage(this.selectedFile, arg0).pipe(
-      catchError((errorMessage) => {
-        this.errorMessage = errorMessage;
-        return EMPTY;
-      })
-    ).subscribe({
-      next: () => {
-        this.increaseCounter();
-        this.levelService.gainExperience(25);
-        this.toastService.success('Imagen subida con éxito, +25 puntos de exp', 'Éxito'); 
-        this.router.navigate(['/scaperooms']);    
-      },
-      error: (error) => {
-        this.toastService.error('Error al subir la imagen', error);
-      },
-    });
-}
-
-    onFileSelected($event: Event) {
-      this.selectedFile = ($event.target as HTMLInputElement).files![0];
-      const maxSize = 2 * 1024 * 1024; // 2MB
-
-      if (this.selectedFile.size > maxSize) {
-        this.toastService.error('La imagen excede el tamaño máximo de 2MB.', 'Error');
-        return;
-      }
-      console.log(this.selectedFile);
-      this.errorMessage= '';
-    }
-
-
-    increaseCounter() {
-    //   const userId = 359; // dinámico si quieres
-    // this.photoService.getPhotosByUser(userId).subscribe((photos) => {
-    //   this.userPhotos.set(photos);
-    //   console.log('Fotos del usuario:', photos);
-    //   console.log(photos.length);
-    //   this.counterService.setCount(photos.length);
-    // });
+increaseCounter() {
+  
     this.counterService.increment(1);
     console.log('Nuevo valor:', this.counterService.count());
   }
 }
+
 
