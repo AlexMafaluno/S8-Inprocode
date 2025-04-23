383a1f25637c5ece9f3aeb75f677e3a47a642b40
Alejandro Martinez <mart.perez93@gmail.com>
2025-04-23 13:25:15 +0200

    fix: correct import path for ModalFilterComponent in ListScaperoomsComponent

--- src/app/components/organisms/list-scaperooms/list-scaperooms.component.ts
+++ src/app/components/organisms/list-scaperooms/list-scaperooms.component.ts
@@ -5,7 +5,7 @@ import { RouterModule } from '@angular/router';
 import { ScaperoomItemComponent } from "../../molecules/scaperoom-item/scaperoom-item.component";
 import { Photo } from '../../../interfaces/photo';
 import { AchivementItemComponent } from "../../molecules/achivement-item/achivement-item.component";
-import { ModalFilterComponent } from "../../modal-filter/modal-filter.component";
+import { ModalFilterComponent } from "../modal-filter/modal-filter.component";
 import { FilterService } from '../../../services/filter.service';
 
 @Component({
